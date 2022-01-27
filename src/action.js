951cecdd87b5b04bacf7f75d62d45695fab5751a
eferreira <eferreira@fanatics.com>
2022-01-27 09:10:24 -0300

    TY on PRs

--- src/action.js
+++ src/action.js
@@ -1,5 +1,21 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
 const run = async () => {
-    console.log('Hello World!!!');
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+
+    const octokit = github.getOctokit(GITHUB_TOKEN);
+
+    const { context = {} } = github;
+    const { pull_request } = context.payload;
+
+    const amount = Math.floor((Math.random())*(5))+1;
+
+    await octokit.issues.createComment({
+        ...context.repo,
+        issue_number: pull_request.number,
+        body: `Thanks for submitting your pull request. If merged this will reward you with ${amount} (fake) ETH`
+    });
 }
 
 run();
